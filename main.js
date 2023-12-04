bd53b9f4efca2431ba9dac65f1ec5cfa6e8dd6bd
Mario Milošević <mariomilosevic887@gmail.com>
2023-12-04 12:13:16 +0100

    Add comments

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ const {
 } = data();
 
 displayCars(app, cars);
-
+// Uzimam auta i dugmad jer ih pravim programatski nakon pozivanja funkcije Displaycars()
 const carDiv = document.querySelectorAll(".car");
 const carDivArr = [...carDiv];
 const carBtn = document.querySelectorAll(".carBtn");
@@ -40,32 +40,32 @@ const carBtnArr = [...carBtn];
 //      console.log(element.innerText);
 //   });
 // };
-
+// Ocisti filter i sort (vrati prvobitne podatke)
 clearBtn.addEventListener("click", function () {
-  clearFilters(app)
-  displayCars(app,cars)
+  clearFilters(app);
+  displayCars(app, cars);
 });
-
+// Prikazi opcije filtera
 filterBtn.addEventListener("click", function () {
   displayOptions(filterOptions);
 });
-
+// Prikazi opcije sorta
 sortBtn.addEventListener("click", function () {
   displayOptions(sortOptions);
 });
-
+// Dostupni
 available.addEventListener("click", function () {
   filter(cars, true, app);
 });
-
+// Nedostupni
 unavailable.addEventListener("click", function () {
   filter(cars, false, app);
 });
-
+// Sortiranje rastuce
 ascending.addEventListener("click", function () {
   sort(cars, "ascending", app);
 });
-
+// Sortiranje opadajuce
 decending.addEventListener("click", function () {
   sort(cars, "decending", app);
 });
@@ -74,6 +74,7 @@ carBtn.forEach((btn) => {
   btn.addEventListener("click", deleteDiv);
 });
 
+// Na klik van filter ili sort, oni postaju nevidljivi
 document.body.addEventListener("click", function (e) {
   focusHandler(e, filterBtn, sortBtn, optionsArray);
 });
